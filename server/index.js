c46da68aea9e7eca694e4136d6432ae774fc797e
PhithLoveSandwich <664259020@webmail.npru.ac.th>
2025-07-21 14:02:44 +0700

    ล่าสุด

--- server/index.js
+++ server/index.js
@@ -1,16 +1,27 @@
-import express from "express"
-const app = express();
+import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
+import restaurantRouter from "./routers/restaurant.router.js";
+
 dotenv.config();
-const PORT = process.env.PORT || 3000;
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors({
+  origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+}));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-  res.send('Restaurant Restful APIS');
+  res.send('Restaurant Restful API');
 });
 
-app.listen(PORT , () => {
-    console.log("litsening too http:\\localhost:" + PORT);
+app.use("/api/v1/restaurant", restaurantRouter);
+
+app.listen(PORT, () => {
+  console.log("Listening to http://localhost:" + PORT);
 });
