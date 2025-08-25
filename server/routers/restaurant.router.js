fb0717d65a6bfa98b8e92aa94cb70d77308d20d5
PhithLoveSandwich <664259020@webmail.npru.ac.th>
2025-08-25 17:12:00 +0700

    จบแล้ว

--- server/routers/restaurant.router.js
+++ server/routers/restaurant.router.js
@@ -1,16 +1,16 @@
 import restaurantController from "../controllers/restaurant.controller.js"; // แก้ชื่อ import
-
+import authMiddleware from "../middleware/authjwt.js"
 import express from "express";
 const router = express.Router();
 
 // POST https://localhost:5000/api/v1/restaurant
-router.post("/", restaurantController.create);
+router.post("/", authMiddleware.verifyToken, authMiddleware.isAdmin , restaurantController.create);
 // GET https://localhost:5000/api/v1/restaurant
 router.get("/",restaurantController.getAll);
 // GET https://localhost:5000/api/v1/restaurant:id
-router.get("/:id",restaurantController.getById);
+router.get("/:id", authMiddleware.verifyToken ,restaurantController.getById);
 // PUT https://localhost:5000/api/v1/restaurant/:id
-router.put("/:id", restaurantController.update);
+router.put("/:id", authMiddleware.verifyToken, authMiddleware.isModOrAdmin , restaurantController.update);
 // DELETE https://localhost:5000/api/v1/restaurant/:id
-router.delete("/:id", restaurantController.deleteById)
+router.delete("/:id", authMiddleware.verifyToken, authMiddleware.isAdmin , restaurantController.deleteById)
 export default router; // แก้ชื่อให้ตรงกับที่ประกาศไว้
